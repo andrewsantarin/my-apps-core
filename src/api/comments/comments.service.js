5b739062a970f5364512ed5043b213635ba47ba1
Cezar Andrew Villegas Santarin <andrew.santarin@gmail.com>
2018-08-20 02:00:54 +0800

    feat(comments): add basic necessary files

---
+++ src/api/comments/comments.service.js
@@ -0,0 +1,27 @@
+import HttpClient from 'lib/fetch/http-client/client';
+import withUri from 'lib/url/with-uri';
+import withQueryString from 'lib/url/with-query-string';
+
+const CommentsService = {
+  read() {
+
+  },
+
+  create() {
+
+  },
+
+  update() {
+
+  },
+
+  save() {
+
+  },
+
+  delete() {
+
+  },
+};
+
+export default CommentsService;
